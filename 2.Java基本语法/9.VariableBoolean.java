b8467852e8874ed1597fcc22337543f832159998
yuanjun <861022418@qq.com>
2020-10-04 22:08:36 +0800

    1004: boolean型

---
+++ 2.Java基本语法/9.VariableBoolean.java
@@ -0,0 +1,14 @@
+class VariableBoolean{
+    public static void main(String[] args) {
+        boolean b1 = true;
+        System.out.println(b1);
+
+        //在条件判断、循环结构中使用
+        boolean isMarried = false;
+        if (isMarried){
+            System.out.println("很遗憾，不能参加单身派对了！");
+        }else{
+            System.out.println("欢迎来参加单身派对!");
+        }
+    }
+}
