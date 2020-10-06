5edfed3cf119d2361611b30fc5679f27e8262e5f
yuanjun <861022418@qq.com>
2020-10-06 21:45:32 +0800

    1006: 比较运算符

---
+++ 3.Java基本语法-运算符/6.CompareOperationExercise.java
@@ -0,0 +1,10 @@
+class CompareOperationExercise{
+    public static void main(String[] args) {
+        boolean b1 = false;
+        if (b1==true)
+            System.out.println("结果为真");
+        else
+            System.out.println("结果为假");
+        //结果为假
+    }
+}
