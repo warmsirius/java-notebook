6a103921626392983c773b7027769fce4df63e99
yuanjun <861022418@qq.com>
2020-10-09 22:24:01 +0800

    1009: for循环增加练习

--- 4.Java基本语法-流程控制/10.ForExercise.java
+++ 4.Java基本语法-流程控制/10.ForExercise.java
@@ -33,24 +33,44 @@ class ForExercise {
 
         //3.最大公约数和最小公倍数
         Scanner scan = new Scanner(System.in);
-        S
+        System.out.println("请输入第1个数字: ");
         int m = scan.nextInt();
+        System.out.println("请输入第2个数字: ");
         int n = scan.nextInt();
-        int maxCommonDivisor = 1;
+
+        //获取最大公约数
+        int maxCommonDivison = 1;
         int smaller = m > n ? n : m;
-        for(int i=2; i<=smaller; i++){
-            if(m%i == 0 && n % i==0){
-                maxCommonDivisor = i;
+        for (int i = smaller; i >= 1; i--) {
+            if (m % i == 0 && n % i == 0) {
+                maxCommonDivison = i;
+                break;//一旦找到了第一个就跳出循环
             }
         }
 
-        if (maxCommonDivisor != 1){
-            System.out.println("最大公约数是: " + maxCommonDivisor);
+        System.out.println("最大公约数是: " + maxCommonDivison);
+
+        //获取最小公倍数
+        int bigger = m < n ? n : m;
+        int smallerCommonMulti = bigger;
+        for (int i = bigger; i <= m * n; i++) {
+            if (i % m == 0 && i % n == 0) {
+                smallerCommonMulti = i;
+                break;
+            }
+        }
+        System.out.println("最小公倍数是: " + smallerCommonMulti);
+
+        //4. 水仙花数
+        for (int i = 153; i < 1000; i++) {
+            int n1 = i / 100;
+            int n2 = i % 100 / 10;
+            int n3 = i % 10;
+            if (i == n1 * n1 * n1 + n2 * n2 * n2 + n3 * n3 * n3) {
+                System.out.println("水仙花数: " + i);
+            }
+
 
         }
-
-
-
-
     }
 }
