557b9f8993d5889ab4b1505ef885f654b9635b51
yuanjun <861022418@qq.com>
2020-12-26 16:13:13 +0800

    fix bug

--- 4.Java基本语法-流程控制/2.IfElse.java
+++ 4.Java基本语法-流程控制/2.IfElse.java
@@ -31,10 +31,10 @@ class IfElse{
         boolean x = true;
         boolean y = false;
         short z = 40;
-        if ((z++==40) && (y=true)){
+        if ((z++ == 40) && (y = true)) {
             z++;
         }
-        if((x=false) || ++z==43){
+        if ((x == false) || ++z == 43) {
             z++;
         }
         System.out.println("z = " + z);
